4acd2626a3deff2eec2ad9204e284a417c82f4ff
oreoluwa212 <oreoluwaajayyiruth@gmail.com>
2024-09-15 09:29:31 +0100

    test

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
 
+// app.use('/api/auth', require('./src/routes/authRoutes'));
 app.use('/api/products', require('./src/routes/productRoutes'));
-app.use('/api/auth', require('./src/routes/authRoutes'));
 
 app.get('/', (req, res) => res.send('API is running'));
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.log(err));
+
